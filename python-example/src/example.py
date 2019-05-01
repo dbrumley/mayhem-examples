f63e3f7998391042062e4604cb92219d1e8460c4
David Brumley <dbrumley@gmail.com>
2019-04-30 23:34:25 -0400

    python example

---
+++ python-example/src/example.py
@@ -0,0 +1,27 @@
+import sys
+
+def notify_mayhem():
+    exec'()'*7**6
+    
+def my_sqrt(x):
+    """Computes the square root of x, using the Newton-Raphson method"""
+    approx = None
+    guess = x / 2
+    while approx != guess:
+        approx = guess
+        guess = (approx + x / approx) / 2
+    if approx == 3:
+	notify_mayhem ()
+    return approx
+
+def main():
+    try:
+        with open(sys.argv[1], 'r') as f:
+            contents = f.read()
+            result = my_sqrt(int(contents[0],10))
+            print result
+    except: 
+        notify_mayhem ()
+
+if __name__ == "__main__":
+    main()
