504c0717beace737ecac42b4b41342289f0e0b36
David Brumley <dbrumley@gmail.com>
2019-04-30 22:17:45 -0400

    nicer name for notifying mayhem

--- ocaml-example/readline.ml
+++ ocaml-example/readline.ml
@@ -1,9 +1,9 @@
-let raise_signal () = Obj.magic 0 0
+let notify_mayhem () = Obj.magic 0 0
 
 let _ =
   let ic = open_in Sys.argv.(1) in
   let s = input_line ic in
   match Array.to_list (Array.init (String.length s) (String.get s)) with
-    ['s'; 'e'; 'c'; 'r'; 'e'; 't'; ' '; 'c'; 'o'; 'd'; 'e'] -> raise_signal ()
-  | ['r'; 'e'; 'c'; 'r'; 'e'; 't'; ' '; 'c'; 'o'; 'd'; 'e'] -> raise_signal ()
+    ['s'; 'e'; 'c'; 'r'; 'e'; 't'; ' '; 'c'; 'o'; 'd'; 'e'] -> notify_mayhem ()
+  | ['r'; 'e'; 'c'; 'r'; 'e'; 't'; ' '; 'c'; 'o'; 'd'; 'e'] -> notify_mayhem ()
   | _ -> ()
