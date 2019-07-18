54bc6b580f7e72cf05d49b6f7a51c8bfd4818384
David Brumley <dbrumley@gmail.com>
2019-07-18 10:23:27 -0400

    added better error handling to install-cert.py

--- helpers/install-cert.py
+++ helpers/install-cert.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-
 import ssl
 import sys
 import socket
@@ -11,9 +9,22 @@ if len(sys.argv) != 2:
   print("Must supply hostname")
   sys.exit(1)
 
+if os.getuid() != 0:
+  print("Must run as root. Try with sudo")
+  sys.exit(1)
+
 hostname=sys.argv[1]
 context = ssl.create_default_context()
-cert=ssl.get_server_certificate((hostname, 443))
+try:
+  cert=ssl.get_server_certificate((hostname, 443))
+except ssl.SSLError as err:
+  print("Failed with SSL Error: %s"%str(err))
+  exit(1)
+except socket.gaierror as err:
+  print("Failed to connect: %s"%str(err))
+  exit(1)
+
+
 try:
   os.makedirs(os.path.join("/etc/docker/certs.d/",hostname ), 0o755)
 except OSError as e:
