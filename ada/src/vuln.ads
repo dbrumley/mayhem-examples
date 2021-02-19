a23db588f08cea56c1acaa8c3145021a5438e88c
David Brumley <dbrumley@forallsecure.com>
2021-02-19 14:57:23 -0500

    fixed ada example

--- ada/src/vuln.ads
+++ ada/src/vuln.ads
@@ -1,33 +1,35 @@
--- This is a simplified ADA example.  
--- Ada supports afl++. This is an example to check binary-only.
--- See https://blog.adacore.com/running-american-fuzzy-lop-on-your-ada-code
--- https://blog.adacore.com/advanced-fuzz-testing-with-aflplusplus-3-00
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Assertions;
 with GNAT.Exception_Actions;
 with Ada.Exceptions;
-with Ada.Command_Line;
-pragma Warnings (Off);
-procedure Fuzzing_Example is
-   procedure Expose_Secrets_On_Correct_Password (Password : access String);
-   procedure Expose_Secrets_On_Correct_Password (Password : access String) is
-      Privileged : Integer := 0;
-   begin
-      if Password.all = "fuzz" then
-	raise Ada.Assertions.Assertion_Error; 
-      else 
-	Put_Line ("Shhhh... the answer to life the universe and everything is 42");
 
-      end if;
-   end Expose_Secrets_On_Correct_Password;
-   Entered_Password : aliased String := Ada.Command_Line.Argument (1);
+procedure Fuzzing_Example is
+   Input : Unbounded_String;
+   Fd : File_Type;
+   Filename : aliased String := Ada.Command_Line.Argument(1);
+   procedure Check_Secret (Password : Unbounded_String) is
+     Fuzz : Unbounded_String;
+   begin
+     Fuzz := To_Unbounded_String("fuzz");
+     if Fuzz = Input then
+        Put_Line ("Fuzz Found!");
+        raise Ada.Assertions.Assertion_Error;
+     else
+        Put_Line ("Not Found!");
+     end if;
+   end Check_Secret;
 begin
-   Expose_Secrets_On_Correct_Password (Password => Entered_Password'Access);
+
+  Open (File => Fd,
+        Mode => In_File,
+        Name => Filename);
+  Input := To_Unbounded_String(Get_Line (FD));
+  Check_Secret(Input);
+
 exception
   when Occurence : others  =>
-     Put_Line ("exception occured [" & Ada.Exceptions.Exception_Name (Occurence)
-               & "] [" & Ada.Exceptions.Exception_Message (Occurence)
-               & "] [" & Ada.Exceptions.Exception_Information (Occurence) & "]");
      GNAT.Exception_Actions.Core_Dump (Occurence);
-
 end Fuzzing_Example;
+
